6e908145a470e8f3f12ca75d38375edd91fc328f
Sri Sudersan <tgsuder@gmail.com>
2026-01-09 02:21:48 -0500

    (feat) : Configure AWS S3 client and utilities

---
+++ lib/s3.ts
@@ -0,0 +1,59 @@
+import {
+  S3Client,
+  PutObjectCommand,
+  DeleteObjectCommand,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { env } from "./env";
+
+const s3Client = new S3Client({
+  region: env.AWS_REGION,
+  credentials: {
+    accessKeyId: env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+const BUCKET_NAME = env.AWS_S3_BUCKET_NAME;
+
+export async function uploadToS3(
+  file: Buffer,
+  key: string,
+  contentType?: string
+): Promise<string> {
+  const command = new PutObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+    Body: file,
+    ContentType: contentType || "image/png",
+  });
+
+  await s3Client.send(command);
+  return key;
+}
+
+export async function deleteFromS3(key: string): Promise<void> {
+  const command = new DeleteObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+  });
+
+  await s3Client.send(command);
+}
+
+export async function getPresignedUrl(
+  key: string,
+  expiresIn: number = 3600
+): Promise<string> {
+  const command = new GetObjectCommand({
+    Bucket: BUCKET_NAME,
+    Key: key,
+  });
+
+  const presignedUrl = await getSignedUrl(s3Client, command, {
+    expiresIn,
+  });
+
+  return presignedUrl;
+}
